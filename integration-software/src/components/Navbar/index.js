da64b9202d00d04654b19d77a3ef2dbf9fbb0829
janessaref <jfong@Janessas-MacBook-Pro.local>
2022-06-15 17:25:10 -0700

    added navbar

--- integration-software/src/components/Navbar/index.js
+++ integration-software/src/components/Navbar/index.js
@@ -4,30 +4,18 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
     return(
-        <>
-        <NavLink to="/" className="navbar-brand pl-3 px-1">Collaborative Solutions</NavLink>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav ml-md-auto">
-                        <li className="nav-item">
-                            <NavLink to="/forecast" className="nav-link links">Forecast</NavLink>
-                        </li>
-                        
-                        <li className="nav-item">
-                            <NavLink to="/actual" className="nav-link links">Actual</NavLink>
-                        </li>
-
-                        <li className="nav-item">
-                            <NavLink to="/test" className="nav-link links">Test</NavLink>
-                        </li>
-
-                    </ul>
-                    <div className="px-3"></div>
-                </div>
-        </>
+        <nav className="navbar">
+            <NavLink 
+                to ="/" 
+                style={({isActive}) =>{
+                    return{color: isActive ? "red" : "grey"};
+                }}
+            >
+                Home
+            </NavLink>
+            <NavLink to ="/forecast">Forecast</NavLink>
+            <NavLink to ="/actual">Actual</NavLink>
+        </nav>
     )
 };
 
